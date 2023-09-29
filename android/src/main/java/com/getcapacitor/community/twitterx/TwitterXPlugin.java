d404fc0d75c320567a880aa254a3b660bd717993
gerrithoskins <gerrit.hoskins@gmail.com>
2023-09-29 13:03:25 +0200

    update android implementation

--- android/src/main/java/com/getcapacitor/community/twitterx/TwitterXPlugin.java
+++ android/src/main/java/com/getcapacitor/community/twitterx/TwitterXPlugin.java
@@ -2,6 +2,7 @@ package com.getcapacitor.community.twitterx;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.util.Log;
 
 import androidx.activity.result.ActivityResult;
 
@@ -19,6 +20,7 @@ import net.openid.appauth.AuthorizationResponse;
 import net.openid.appauth.AuthorizationService;
 import net.openid.appauth.AuthorizationServiceConfiguration;
 import net.openid.appauth.ResponseTypeValues;
+import net.openid.appauth.TokenRequest;
 
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
@@ -27,34 +29,66 @@ import java.util.concurrent.Executors;
 @CapacitorPlugin(name = "TwitterX")
 public class TwitterXPlugin extends Plugin {
     public static final String LOG_TAG = "twitterX ";
+    private static final String AUTHORIZATION_ENDPOINT = "https://twitter.com/i/oauth2/authorize";
+    private static final String TOKEN_ENDPOINT = "https://api.twitter.com/2/oauth2/token";
     private AuthState authState;
     private AuthorizationService authService;
+    private AuthorizationServiceConfiguration config;
+
+    private String accessToken = "";
 
     @PluginMethod
     public void login(final PluginCall call) {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.execute(() -> {
             try {
-                AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(
-                        Uri.parse("https://api.twitter.com/oauth/authorize"),
-                        Uri.parse("https://api.twitter.com/oauth/token")
+                config = new AuthorizationServiceConfiguration(
+                        Uri.parse(AUTHORIZATION_ENDPOINT),
+                        Uri.parse(TOKEN_ENDPOINT)
                 );
 
-                AuthorizationRequest request = new AuthorizationRequest.Builder(
-                        config,
-                        getConfig().getString("clientId"),
-                        ResponseTypeValues.CODE,
-                        Uri.parse(getConfig().getString("redirectUri"))
-                )
-                        .setScope(getConfig().getString("scope"))
-                        .build();
-
+                authState = new AuthState(config);
                 authService = new AuthorizationService(getContext());
-                Intent authIntent = authService.getAuthorizationRequestIntent(request);
-                startActivityForResult(call, authIntent, "handleOauthIntentResult");
+
+                if(authState.getNeedsTokenRefresh() && !accessToken.isEmpty()) {
+                    performTokenRequest();
+                } else {
+                    AuthorizationRequest request = new AuthorizationRequest.Builder(
+                            config,
+                            getConfig().getString("clientId"),
+                            ResponseTypeValues.CODE,
+                            Uri.parse(getConfig().getString("redirectUri"))
+                    )
+                            .setScope(getConfig().getString("scope"))
+                            .build();
+
+                    //authService = new AuthorizationService(getContext());
+                    Intent authIntent = authService.getAuthorizationRequestIntent(request);
+                    startActivityForResult(call, authIntent, "handleOauthIntentResult");
+                }
             } catch (Exception exception) {
                 exception.printStackTrace();
                 call.reject(LOG_TAG + "Unexpected exception on open browser for authorization request.", exception);
+            }
+        });
+    }
+
+    public void performTokenRequest() {
+        String clientId = getConfig().getString("clientId");
+
+        TokenRequest tokenRequest = new TokenRequest.Builder(config, clientId)
+                .setGrantType("refresh_token")
+                //.setRefreshToken("bWRWa3gzdnk3WHRGU1o0bmRRcTJ5VUxWX1lZTDdJSUtmaWcxbTVxdEFXcW5tOjE2MjIxNDc3NDM5MTQ6MToxOnJ0OjE")
+                .setRefreshToken(accessToken)
+                .build();
+
+        authService.performTokenRequest(tokenRequest, (tokenResponse, exception) -> {
+            if (exception != null) {
+                Log.e("AppAuth", "Token Request failed", exception);
+            } else if (tokenResponse != null) {
+                // Update AuthState with the Token Response
+                authState.update(tokenResponse, null);
+                accessToken = tokenResponse.accessToken;
             }
         });
     }
@@ -83,7 +117,7 @@ public class TwitterXPlugin extends Plugin {
 
     @ActivityCallback
     private void handleOauthIntentResult(PluginCall call, ActivityResult result) {
-        if (result.getResultCode() == 0) {
+        if (result.getResultCode() != 0) {
             Intent data = result.getData();
             if (data != null) {
                 AuthorizationResponse response = AuthorizationResponse.fromIntent(data);
